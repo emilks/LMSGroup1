785afd6f47c713d1420425353d8111cedee5483d
Eknah <eknah@protonmail.com>
2022-09-07 14:56:45 +0200

    Scaffold entity controllers

--- LMS.Web/Data/ApplicationDbContext.cs
+++ LMS.Web/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using LMS.Core.Entities;
 
 namespace LMS.Web.Data
 {
@@ -9,5 +10,10 @@ namespace LMS.Web.Data
             : base(options)
         {
         }
+        public DbSet<LMS.Core.Entities.Course>? Course { get; set; }
+        public DbSet<LMS.Core.Entities.Module>? Module { get; set; }
+        public DbSet<LMS.Core.Entities.Activity>? Activity { get; set; }
+        public DbSet<LMS.Core.Entities.ActivityType>? ActivityType { get; set; }
+        public DbSet<LMS.Core.Entities.Document>? Document { get; set; }
     }
 }
