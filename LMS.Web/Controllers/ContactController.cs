6239d2bae7ea8f0c1c2ac291b6e734777a9753fc
Dennis Järnstål <dennis_jarnstal@hotmail.com>
2022-09-08 13:59:14 +0200

    Added and updated controllers for new functions

---
+++ LMS.Web/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using LMS.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LMS.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
