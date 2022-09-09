cc1b4045f4ce43f744a1f6b91c30dc1b7c0c87a8
Eknah <eknah@protonmail.com>
2022-09-09 13:29:15 +0200

    Add attributes

--- LMS.Core/Entities/Module.cs
+++ LMS.Core/Entities/Module.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +13,23 @@ namespace LMS.Core.Entities
     public class Module
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 3)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(80, MinimumLength = 3)]
         public string Description { get; set; } = string.Empty;
+
+        [DisplayName("Start date")]
         public DateTime StartDate { get; set; }
+
+        [DisplayName("End date")]
         public DateTime EndDate { get; set; }
+
+        // Foreign keys
+        //public int CourseId { get; set; }
 
         // Navigation props
         public ICollection<Activity> Activities { get; set; } = new List<Activity>();
