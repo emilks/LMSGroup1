9d47bf12c1a9d32ac9325ce76138399b82395563
Eknah <eknah@protonmail.com>
2022-09-16 13:41:38 +0200

    Make start and end date Date-only

--- LMS.Core/ViewModels/ModuleViewModel.cs
+++ LMS.Core/ViewModels/ModuleViewModel.cs
@@ -2,6 +2,7 @@
 using LMS;
 using LMS.Core.Validations;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace LMS.Core.ViewModels
 {
@@ -10,11 +11,13 @@ namespace LMS.Core.ViewModels
         public string Name { get; set; }
         public string Description { get; set; }
         [Remote(action: "VerifyStartDate", controller: "Modules")]
+        [DataType(DataType.Date)]
         // Avvaktivera backend-validering tills svidare
         //[ValidateModuleStartDate]
         public DateTime StartDate { get; set; }
         // Avvaktivera backend-validering tills svidare
         //[ValidateModuleEndDate]
+        [DataType(DataType.Date)]
         [Remote(action: "VerifyEndDate", controller: "Modules", AdditionalFields = "StartDate,CourseId")]
         public DateTime EndDate { get; set; }
         public IEnumerable<ActivityViewModel> Activities { get; set; }
