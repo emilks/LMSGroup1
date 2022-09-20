70a95ea03ed9c0ae123d415dcaa9988680517692
emilk <emilks@gmail.com>
2022-09-20 10:50:07 +0200

    Knappar

--- LMS.Core/ViewModels/ActivityViewModel.cs
+++ LMS.Core/ViewModels/ActivityViewModel.cs
@@ -1,13 +1,31 @@
 ﻿#nullable disable
 using LMS;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LMS.Core.ViewModels
 {
     public class ActivityViewModel
     {
+        [DisplayName("Modulnamn")]
         public string Name { get; set; }
+        [DisplayName("Beskrivning")]
         public string Description { get; set; }
+        [DisplayName("Startdatum")]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        [DisplayName("Längd (antal dagar)")]
+        public int Duration
+        {
+            get
+            {
+                return (EndDate - StartDate).Days;
+            }
+            set
+            {
+                EndDate = StartDate + TimeSpan.FromDays(value);
+            }
+        }
     }
 }
