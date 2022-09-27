a878a0c51a2c9862bc04b0ef3d9b81ad7867f19a
Lexicon Student 1 <Lexicon Student 1@DESKTOP-OF8V11H>
2022-09-27 17:07:53 +0200

    add migration

--- LMS.Data/Migrations/20220927150618_init.Designer.cs
+++ LMS.Data/Migrations/20220927150618_init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LMS.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20220927145411_init")]
+    [Migration("20220927150618_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
